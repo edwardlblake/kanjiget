02a7fdb9b19433eac4724fad71dfb6dd29367b9c
Edward L. Blake <edwardlblake@gmail.com>
2012-08-25 18:38:11 -0400

    using macro

--- wiktionarytemplates.rkt
+++ wiktionarytemplates.rkt
@@ -4,31 +4,58 @@
 
 (define (get-wiktionary-templates)
   (define templates (make-hash))
-  (hash-set! templates "sense" 
-             (lambda (args)
-               (format "(''~a''):" (car args))
-               ))
   
-  '(all the templates to look into implementing
+  (define-syntax wikt-templates-table
+    (syntax-rules (* show-as)
+      [(wikt-templates-table * num classname (show-as fmt args argx) rst ...)
+       (let ()
+         (hash-set! templates classname
+                     (lambda (args)
+                       (format fmt . argx)
+                       ))
+         (wikt-templates-table rst ...))]
+      
+      [(wikt-templates-table * num classname _ (show-as fmt args argx) rst ...)
+       (let ()
+         (hash-set! templates classname
+                     (lambda (args)
+                       (format fmt . argx)
+                       ))
+         (wikt-templates-table rst ...))]
+      
+      [(wikt-templates-table a rst ...)
+       (wikt-templates-table rst ...)
+       ]
+      [(wikt-templates-table)
+       (void)
+       ]))
+    
+  (wikt-templates-table all the templates to look into implementing
         Occurence  Template name
         ---------  -------------
-        47227	"defn"
+        * 47227	"defn"        
+        (show-as "" _ ())
         (example "{{defn|lang=yue|sort=高00}}"
                  is hidden -- probably meta-data)
         
-        20907	"hani-forms"
+        * 20907	"hani-forms"
+        TODO
         
-        18171	"ja-kanjitab"
+        * 18171	"ja-kanjitab"
+        TODO
         (example "{{ja-kanjitab|高}}"
                  (into "Kanji in this term: 高"))
         
-        14479	"cmn-noun"
+        * 14479	"cmn-noun"
+        TODO
         
-        13011	"ja-noun"
+        * 13011	"ja-noun"
+        TODO
         (example "{{ja-noun|k|hira=たか|rom=taka}}"
                  (into "高 (''hiragana'' ''[[たか]]'', ''romaji'' '''taka''')"))
         
-        12599	"han ref"
+        * 12599	"han ref"
+        TODO
         (example "{{Han ref|kx=1451.260|dkj=45313|dj=1979.040|hdz=74593.010|uh=9AD8|ud=39640|bh=B0AA|bd=45226}}"
                  (into "=== References ==="
                        "* KangXi: page 1451, character 26"
@@ -37,147 +64,196 @@
                        "* Hanyu Da Zidian: volume 7, page 4593, character 1"
                        "* Unihan data for U+9AD8"))
         
-        12598	"han char"
+        * 12598	"han char"
+        TODO
         (example "{{Han char|rn=189|rad=高|as=00|sn=10|four=0022<sub>7</sub>|canj=卜口月口 (YRBR)|ids=⿳[[亠]][[口]][[冋]]}}"
                  (into "高 (''radical 189'' [[高]]+0, 10 ''strokes'', ''cangjie input'' 卜口月口 (YRBR), ''four-corner'' 0022<sub>7</sub>, ''composition'' ⿳[[亠]][[口]][[冋]])"))
         
-        12254	"cmn-hanzi"
+        * 12254	"cmn-hanzi"
+        TODO
         (example "{{cmn-hanzi|pin=[[gāo]] ([[gao1]])|wg=kao<sup>1</sup>}}"
                  (into "高 (pinyin [[gāo]] ([[gao1]]), ''Wade-Giles'' kao<sup>1</sup>)"))
         
-        12252	"ja-readings"
+        * 12252	"ja-readings"
+        TODO
         (example "{{ja-readings|on=[[こう]] (kō)|kun=[[たかい]] (takai), [[たかまる]] (takamaru), [[たかさ]] (takasa)}}"
                  (into "* '''On:''' [[こう]] (kō)"
                        "* '''Kun:''' [[たかい]] (takai), [[たかまる]] (takamaru), [[たかさ]] (takasa)"))
         
-        12251	"ja-kanji"
+        * 12251	"ja-kanji"
+        TODO
         (example "{{ja-kanji|grade=2|rs=高00}}"
                  (into "高 (''grade 2 “Kyōiku” kanji'')"))
         
-        12151	"l"
-        10008	"reference-book"
-        8924	"cite web"
+        * 12151	"l"
+        TODO
+        * 10008	"reference-book"
+        TODO
+        * 8924	"cite web"
+        TODO
         
-        8802	"yue-hanzi"
+        * 8802	"yue-hanzi"
+        TODO
         (examples "{{yue-hanzi|jyut=|y=gou1}}"
                   (into "高 (''Yale'' gou1)"))
         
-        8176	"ko-hanja"
+        * 8176	"ko-hanja"
+        TODO
         (example "{{ko-hanja|hangeul=고|eumhun=높을|rv=go|mr=ko|y=ko}}"
                  (into "高"
                        "'''Eumhun:'''"
                        "'''Sound''' (''hangeul''): [[고]] (''revised:'' go, ''McCune-Reischauer:'' ko, ''Yale:'' ko)"
                        "'''Name''' (''hangeul''): [[높을]]()"))
         
-        7797	"zh-hanzi"
+        * 7797	"zh-hanzi"
+        TODO
         
-        6843	"ja-pos"
+        * 6843	"ja-pos"
+        TODO
         (example "{{ja-pos|k|particle|rom=dake|hira=だけ|hidx=たけ'}}"
                  (into "丈 (''hiragana'' [[だけ]], ''romaji'' '''dake''')"))
         
-        6418	"ipa"
-        6364	"cmn-verb"
-        5104	"also"
+        * 6418	"ipa"
+        TODO
+        * 6364	"cmn-verb"
+        TODO
+        * 5104	"also"
+        TODO
         (example "{{also|嶽}}"
                  (into ":See also [[嶽]]"))
         
-        3701	"term"
+        * 3701	"term"
+        TODO
         (example "{{term|高|taka|height}}"
                  (into "''[[高|taka]]'' (“height”)"))
         
-        3666	"given name"
+        * 3666	"given name"
+        TODO
         (example "{{ja-kanji|grade=c|rs=一02}}"
                  (into "丈 (''hiragana'' [[じょう]], ''romaji'' '''Jō''')"
                        "# A male given name"))
         
-        3288	"audio"
-        (cant have this)
+        * 3288	"audio"
+        (show-as "" _ ())
         
-        3139	"vi-hantu"
+        * 3139	"vi-hantu"
+        TODO
         (example "{{vi-hantu|[[cao]], [[sào]]|rs=高00}}"
                  (into "高 ([[cao]], [[sào]])"))
         
-        3036	"literary"
-        3034	"pedialite"
-        2976	"cmn-proper noun"
+        * 3036	"literary"
+        TODO
+        * 3034	"pedialite"
+        TODO
+        * 2976	"cmn-proper noun"
+        TODO
         
-        2732	"archaic"
+        * 2732	"archaic"
+        TODO
         (example "{{archaic|lang=mul}}"
                  (into "(''archaic'')"))
         
-        2371	"cmn-adj"
-        1839	"cmn-idiom"
-        1721	"advanced mandarin"
-        1507	"ja-verb"
+        * 2371	"cmn-adj"
+        TODO
+        * 1839	"cmn-idiom"
+        TODO
+        * 1721	"advanced mandarin"
+        TODO
+        * 1507	"ja-verb"
+        TODO
         (example "{{ja-verb|kk|type=2|hira=たける|rom=takeru|hidx=たける}}"
                  (into "長ける (''ichidan conjugation'', ''hiragana'' [[たける]], ''romaji'' '''takeru''')"))
         
-        1458	"han compound"
+        * 1458	"han compound"
+        TODO
         (example "{{Han compound|丘|山}}"
                  (into "[[丘]] + [[山]]"))
         
-        1239	"surname"
-        1176	"ja-verb-suru"
-        1134	"wikipedia"
-        1078	"a"
-        955	"cmn-adv"
-        891	"usex"
-        871	"ja-suru"
+        * 1239	"surname"
+        TODO
+        * 1176	"ja-verb-suru"
+        TODO
+        * 1134	"wikipedia"
+        TODO
+        * 1078	"a"
+        TODO
+        * 955	"cmn-adv"
+        TODO
+        * 891	"usex"
+        TODO
+        * 871	"ja-suru"
+        TODO
         
-        844	"ja-adj"
+        * 844	"ja-adj"
+        TODO
         (example "{{ja-adj|kk|decl=i|hira=たかい|rom=takai}}"
                  (into "高い (い-i declension, ''hiragana'' [[たかい]], ''romaji'' '''takai''')"))
         
-        825	"qualifier"
-        805	"elementary mandarin"
-        783	"bottom"
-        756	"context"
-        738	"stroke order"
-        714	"nan-noun"
-        681	"intermediate mandarin"
-        560	"ja-see-also"
-        530	"rfe"
-        529	"zh-ts"
-        522	"beginning mandarin"
+        * 825	"qualifier"
+        TODO
+        * 805	"elementary mandarin"
+        TODO
+        * 783	"bottom"
+        TODO
+        * 756	"context"
+        TODO
+        * 738	"stroke order"
+        TODO
+        * 714	"nan-noun"
+        TODO
+        * 681	"intermediate mandarin"
+        TODO
+        * 560	"ja-see-also"
+        TODO
+        * 530	"rfe"
+        TODO
+        * 529	"zh-ts"
+        TODO
+        * 522	"beginning mandarin"
+        TODO
         
-        506	"sense"
+        * 506	"sense"
+        (show-as "(''~a''):" lst ((car lst)))
         
-        454	"cmn-proverb"
-        432	"ja-na"
-        407	"mid3"
-        406	"compound"
-        404	"etyl"
-        370	"suffix"
-        368	"slang"
-        368	"ja-ichi"
-        363	"alternative form of"
-        349	"top2"
-        341	"jpan"
-        332	"mid2"
-        317	"head"
-        297	"nan-verb"
-        296	"ja-forms"
-        286	"t"
-        278	"ko-noun"
-        277	"ko-hanjatab"
-        267	"mid4"
-        251	"ja-go-ru"
-        244	"ja-def"
-        243	"hanja form of"
-        239	"attention"
-        237	"zh-attention"
-        220	"ja-i" "declension of X" wont bother
-        210	"colloquial"
-        203	"top3"
-        199	"han simp"
-        198	"idiomatic"
-        186	"cmn-interj"
-        185	"liushu"
-        183	"yue-noun"
-        175	"py-to-ipa"
-        173	"t-"
-        173	"ja"
-        165	"cmn"
+        * 454	"cmn-proverb"
+        * 432	"ja-na"
+        * 407	"mid3"
+        * 406	"compound"
+        * 404	"etyl"
+        * 370	"suffix"
+        * 368	"slang"
+        * 368	"ja-ichi"
+        * 363	"alternative form of"
+        * 349	"top2"
+        * 341	"jpan"
+        * 332	"mid2"
+        * 317	"head"
+        * 297	"nan-verb"
+        * 296	"ja-forms"
+        * 286	"t"
+        * 278	"ko-noun"
+        * 277	"ko-hanjatab"
+        * 267	"mid4"
+        * 251	"ja-go-ru"
+        * 244	"ja-def"
+        * 243	"hanja form of"
+        * 239	"attention"
+        * 237	"zh-attention"
+        
+        * 220	"ja-i"
+        (show-as "" _ ())
+        
+        * 210	"colloquial"
+        * 203	"top3"
+        * 199	"han simp"
+        * 198	"idiomatic"
+        * 186	"cmn-interj"
+        * 185	"liushu"
+        * 183	"yue-noun"
+        * 175	"py-to-ipa"
+        * 173	"t-"
+        * 173	"ja"
+        * 165	"cmn"
         )
   
   templates
